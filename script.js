fcc844f90cefd5f3a0368c4bd19f7bc603ebcd69
Balogunvinh <balogunvinh@gmail.com>
2023-04-15 01:29:59 +0100

    login page with javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+let signupBtn = document.getElementById("signupBtn");
+let signInBtn = document.getElementById("signInBtn");
+let title = document.getElementById("title");
+let nameField = document.getElementById("nameField");
+
+signInBtn.onclick = function(){
+    nameField.style.maxHeight = "0";
+    title.innerHTML = 'Sign In';
+    signupBtn.classList.add("disable");
+    signInBtn.classList.remove("disable");
+}
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = "60px";
+    title.innerHTML = 'Sign Up';
+    signupBtn.classList.remove("disable");
+    signInBtn.classList.add("disable");
+}
